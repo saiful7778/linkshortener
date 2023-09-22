b6a7ff87957bf635754bf474229f8b5c39dadec5
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 00:34:10 +0600

    setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/style.css';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import { RouterProvider } from 'react-router-dom';
+import Redirect from './pages/Redirect';
+
+const router = createBrowserRouter([
+  {
+    path: "/linkshortener/",
+    element: <Home />
+  },
+  {
+    path: "/linkshortener/:linkid",
+    element: <Redirect />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
