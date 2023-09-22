bf04cfe98c57c98f593917aa4a45def91f117492
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 01:22:35 +0600

    add input box

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
-import './style.css';
-
 const Home = () => {
   return (
     <div className='main-bg'>
-      <div className="container w-4/5 mx-auto p-3"></div>
+      <div className="container w-3/4 mx-auto p-3">
+        <div className="container-box p-3">
+          <div className="flex items-center gap-3">
+            <div className="flex-1">
+              <input className="input w-full" type="url" placeholder="Enter url...." />
+            </div>
+            <button className="btn" type="button">add</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
