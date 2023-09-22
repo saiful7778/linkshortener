c7ee84472a1f646f8e9056fda73d2438f6d15f4e
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 02:00:43 +0600

    add theme swither

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react'
+import { FiMoon, FiSun } from 'react-icons/fi'
+import { setItemData } from '../../utility/MgLocalStore'
+import { useEffect } from 'react';
+
 const Home = () => {
+  /**
+   * theme state true means dark mode and false means light mode
+   */
+  const [theme, setTheme] = useState(false);
+  useEffect(() => {
+    const prevTheme = localStorage.getItem('siteTheme') === 'dark' ? true : false;
+    setTheme(prevTheme)
+  }, []);
+  const handleTheme = () => {
+    setTheme(prop => !prop)
+    if (!theme) {
+      setItemData('siteTheme', 'dark')
+    } else {
+      setItemData('siteTheme', 'light')
+    }
+  }
   return (
-    <div className='main-bg'>
-      <div className="container w-3/4 mx-auto p-3">
-        <div className="container-box px-4 py-3">
-          <div className="flex items-center gap-3">
-            <div className="flex-1">
-              <input className="input w-full" type="url" placeholder="Enter url...." />
+    <div className={`${theme ? 'dark' : 'light'}`}>
+      <div className='main-bg'>
+        <div className="container w-3/4 mx-auto p-3">
+          <div className="container-box px-4 py-3 relative">
+            <div className="flex items-center gap-3">
+              <div className="flex-1">
+                <input className="input w-full" type="url" placeholder="Enter url...." />
+              </div>
+              <button className="btn" type="button">add</button>
             </div>
-            <button className="btn" type="button">add</button>
+            <div className="absolute -right-12 top-1/2 -translate-y-1/2 z-10">
+              <button onClick={handleTheme} className='icon-btn' type="button">
+                {theme ? <FiSun /> : <FiMoon />}
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="container-box mt-4 px-4 py-3">
-          <h1 className="text-3xl font-semibold">All links here:</h1>
+          <div className="container-box mt-4 px-4 py-3">
+            <h1 className="text-3xl font-semibold">All links here:</h1>
+          </div>
         </div>
       </div>
     </div>
