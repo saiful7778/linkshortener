500bd586cb95e78df8445d0b98752cd808a7efe3
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 01:02:32 +0600

    add main homepage background

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Home = () => {
+  return (
+    <div className='main-bg'>
+      <div className="container w-4/5 mx-auto p-3"></div>
+    </div>
+  )
+}
+
+export default Home
