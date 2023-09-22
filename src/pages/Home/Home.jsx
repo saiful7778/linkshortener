82d095a609803aaa1668dee3008428c9f1f266ea
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 01:28:37 +0600

    add all links container and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,16 @@ const Home = () => {
   return (
     <div className='main-bg'>
       <div className="container w-3/4 mx-auto p-3">
-        <div className="container-box p-3">
+        <div className="container-box px-4 py-3">
           <div className="flex items-center gap-3">
             <div className="flex-1">
               <input className="input w-full" type="url" placeholder="Enter url...." />
             </div>
             <button className="btn" type="button">add</button>
           </div>
+        </div>
+        <div className="container-box mt-4 px-4 py-3">
+          <h1 className="text-3xl font-semibold">All links here:</h1>
         </div>
       </div>
     </div>
