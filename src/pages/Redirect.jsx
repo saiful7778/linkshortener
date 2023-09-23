303b3b527c47764e5af58910bb61c51e0f9733a9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-23 13:52:23 +0600

    add redirect page functions

--- src/pages/Redirect.jsx
+++ src/pages/Redirect.jsx
@@ -1,10 +1,18 @@
 import { useParams } from "react-router-dom"
+import { getItemData } from '@/utility/MgLocalStore';
+
 
 const Redirect = () => {
   const { linkid } = useParams()
-  console.log(linkid);
+  const remain = getItemData("allshortlinks");
+  let redirectLink = '';
+  if (remain) {
+    const remaining = remain.filter((ele) => (ele.id === linkid ? ele : ""));
+    redirectLink = remaining[0].value;
+    window.location.href = redirectLink;
+  }
   return (
-    <div>Redirect</div>
+    <div>Redirect to {redirectLink}</div>
   )
 }
 
